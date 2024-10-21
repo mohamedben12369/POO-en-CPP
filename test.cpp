ca12bef4de225ceccffeba19c6d602ca37e7e7c1
mohamedben12369 <mohamedbenh7.com>
2024-10-21 02:06:17 +0100

    my first commit

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+void processNumbers(const std::vector<int>& numbers) {
+    int sum = 0;
+    for (int number : numbers) {
+        sum += number; // Traitement
+    }
+    std::cout << "Sum: " << sum << std::endl;
+}
+
+int main() {
+    std::vector<int> myNumbers = {1, 2, 3, 4, 5, 6}; // Liste d'arguments
+    processNumbers(myNumbers); 
+    std::cout<<"hello";// Appel avec un seul argument de type vecteur
+    return 0;
+}
