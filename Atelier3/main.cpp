03bdfecc2d9ede7e5c9ec54ae1f2839e6ce64061
mohamedben12369 <mohamedbenh7.com>
2024-10-21 09:28:16 +0100

    sd

--- Atelier3/main.cpp
+++ Atelier3/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+using namespace std;
+class voiture
+{   public:
+    string modele;
+    string marque;
+    int annedecreation;
+     
+    float kilometrage;
+    float vitesseactuelle;
+    voiture();
+   
+    };
+voiture::voiture(){
+//constructor par defaut si l'utilisateur a oublie les donnes de voiture par exemple
+   
+        
+        modele="aucun";
+        marque="aucun";
+        annedecreation=2010;
+        kilometrage=4500;
+        vitesseactuelle=60;
+   
+};
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main()
+{
+ voiture cetroin;
+ cout<<cetroin.modele;
+ return 0;
 }
